3067fc7550c2725cbc9d683737c83361ec9cf750
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2021-03-02 15:40:59 +0100

    redact passwords in errors

--- src/lib/withErrorResponse.js
+++ src/lib/withErrorResponse.js
@@ -1,10 +1,24 @@
 const { inspect } = require("util");
+const fastRedact = require("fast-redact");
+
+const redact = fastRedact({
+  paths: ["*.password"],
+  serialize: (o) => inspect(o, { depth: 3 }),
+});
+
+const safeParse = (string) => {
+  try {
+    return JSON.parse(string);
+  } catch (error) {
+    return "invalid JSON";
+  }
+};
 
 module.exports.withErrorResponse = (handler) => async (event, context) => {
   try {
     return await handler(event, context);
   } catch (error) {
-    console.error(inspect(event, { depth: 3 }));
+    console.error(redact({ ...event, body: safeParse(event.body) }));
     console.error(error);
     return {
       statusCode: error.code || 500,
